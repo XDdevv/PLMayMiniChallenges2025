b68c6e77752b7b0f3f2053a298987dc2b1e82e36
XDdevv <undefineduser087@gmail.com>
2025-05-08 21:40:51 +0500

    Daily word card challenge's done

--- app/src/main/java/zed/rainxch/plmayminichallenges2025/daily_word_challenge/presentation/MainActivity.kt
+++ app/src/main/java/zed/rainxch/plmayminichallenges2025/daily_word_challenge/presentation/MainActivity.kt
@@ -1,4 +1,40 @@
-package zed.rainxch.plmayminichallenges2025.daily_word_challenge.presentation
-
-class MainActivity {
-}
+//package zed.rainxch.plmayminichallenges2025
+//
+//import android.os.Bundle
+//import android.speech.tts.TextToSpeech
+//import androidx.activity.ComponentActivity
+//import androidx.activity.compose.setContent
+//import androidx.activity.enableEdgeToEdge
+//import androidx.compose.foundation.layout.fillMaxSize
+//import androidx.compose.foundation.layout.padding
+//import androidx.compose.material3.Scaffold
+//import androidx.compose.material3.Text
+//import androidx.compose.runtime.Composable
+//import androidx.compose.ui.Modifier
+//import androidx.compose.ui.tooling.preview.Preview
+//import zed.rainxch.plmayminichallenges2025.daily_word_challenge.domain.speaker.TTSManager
+//import zed.rainxch.plmayminichallenges2025.daily_word_challenge.presentation.DailyWordChallenge
+//import zed.rainxch.plmayminichallenges2025.ui.theme.PLMayMiniChallenges2025Theme
+//
+//class MainActivity : ComponentActivity() {
+//
+//    lateinit var ttsManager: TTSManager
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        enableEdgeToEdge()
+//        ttsManager = TTSManager(this)
+//        setContent {
+//            PLMayMiniChallenges2025Theme {
+//                DailyWordChallenge(onSpeakClicked = {
+//                    ttsManager.speak(it)
+//                })
+//            }
+//        }
+//    }
+//
+//    override fun onDestroy() {
+//        super.onDestroy()
+//        ttsManager.shutdown()
+//    }
+//}
